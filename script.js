7f6dbd940054d41d8c3c0fcfce6f8d5546aae690
Jack Bridger <jacksbridger@gmail.com>
2019-04-26 13:31:39 +0900

    completely changed - brought it over from the second site I'd been working on under mark2.html

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-    var i = 0,
-        images = ['images/codeatuni11.jpg','images/codeatuni12.jpg','images/codeatuni13.jpg','images/codeatuni14.jpg'],
-        speed = 2500,
-        moving = true,
-        timer = speed;
+var i = 0,
+images = ['images_2/codeatuni2_1.jpg','images_2/codeatuni2_3.jpg','images_2/codeatuni2_5.jpg','images_2/codeatuni2_6.jpg'],
+speed = 2500,
+moving = true,
+timer = speed;
 
-    const num_images = images.length;
+const num_images = images.length;
 
-    function changeImg(){
-        clearTimeout(timer);
-        timer = setTimeout(changeImg,speed);
-        if (moving) {
-            document.slide.src = images[i];
-            i < (num_images -1) ? i++ : i = 0;}
-        else
-            clearTimeout(timer);
-    }
+function changeImg(){
+clearTimeout(timer);
+timer = setTimeout(changeImg,speed);
+if (moving) {
+    document.slide.src = images[i];
+    i < (num_images -1) ? i++ : i = 0;}
+else
+    clearTimeout(timer);
+}
 
-    function nextImg() {
-        i++;
-        i = i % num_images;
-        document.slide.src = images[i];
-    }
+function nextImg() {
+i++;
+i = i % num_images;
+document.slide.src = images[i];
+}
 
-    function prevImg() {
-        if (i > 0)
-            i--;
-        else
-            i = num_images -1;
+function prevImg() {
+if (i > 0)
+    i--;
+else
+    i = num_images -1;
 
-        document.slide.src = images[i];
-    }
+document.slide.src = images[i];
+}
 
-    function pausePlay() {
-        if (!moving) {
-            document.getElementById("pauseslider").innerText = 'Pause'}
-        else {
-            document.getElementById("pauseslider").innerText = 'Play'
-            i--;
-        }
+function pausePlay() {
+if (!moving) {
+    document.getElementById("pauseslider").innerText = 'Pause'}
+else {
+    document.getElementById("pauseslider").innerText = 'Play'
+    i--;
+}
 
-        moving = !moving;
-        changeImg();
-    }
+moving = !moving;
+changeImg();
+}
 
-    document.onkeydown = function(e) {
-        e = e || window.event;
-        if (e.keyCode == '37') {
-            prevImg();}
-        else if (e.keyCode == '39') {
-            nextImg();}
-    }
+document.onkeydown = function(e) {
+e = e || window.event;
+if (e.keyCode == '37') {
+    prevImg();}
+else if (e.keyCode == '39') {
+    nextImg();}
+}
 
 
-    window.onload = changeImg;
+window.onload = changeImg;
