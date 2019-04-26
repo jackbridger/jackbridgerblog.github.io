3a2a46bfae13c8a26d269a66f0330bd5d8cc2051
Jack Bridger <jacksbridger@gmail.com>
2019-04-26 11:39:01 +0900

    added the slideshow

---
+++ mark2/mark2script.js
@@ -0,0 +1,55 @@
+var i = 0,
+images = ['images_2/codeatuni2_1.jpg','images_2/codeatuni2_3.jpg','images_2/codeatuni2_5.jpg','images_2/codeatuni2_6.jpg'],
+speed = 2500,
+moving = true,
+timer = speed;
+
+const num_images = images.length;
+
+function changeImg(){
+clearTimeout(timer);
+timer = setTimeout(changeImg,speed);
+if (moving) {
+    document.slide.src = images[i];
+    i < (num_images -1) ? i++ : i = 0;}
+else
+    clearTimeout(timer);
+}
+
+function nextImg() {
+i++;
+i = i % num_images;
+document.slide.src = images[i];
+}
+
+function prevImg() {
+if (i > 0)
+    i--;
+else
+    i = num_images -1;
+
+document.slide.src = images[i];
+}
+
+function pausePlay() {
+if (!moving) {
+    document.getElementById("pauseslider").innerText = 'Pause'}
+else {
+    document.getElementById("pauseslider").innerText = 'Play'
+    i--;
+}
+
+moving = !moving;
+changeImg();
+}
+
+document.onkeydown = function(e) {
+e = e || window.event;
+if (e.keyCode == '37') {
+    prevImg();}
+else if (e.keyCode == '39') {
+    nextImg();}
+}
+
+
+window.onload = changeImg;
