3e95344126a5e6ac66bdb9d97840096ac66d678d
Jack Bridger <jacksbridger@gmail.com>
2019-05-27 16:51:48 +0100

    there is a failing test that I didn't write. But can't find it

--- calculator/new_calc.js
+++ calculator/new_calc.js
@@ -1,37 +1,25 @@
+const test = require('tape').test;
+
 class Calculator {
     constructor(){
-        // this.displayMain = document.getElementByClassName('display-main');
+        this.displayMain = null;
     }
 
     clear() {
-        console.log('cleared');
-        var displayMain = '0';
-
+        this.displayMain = '0';
     }
 
 };
 
-const calculator = new Calculator();
-
-
-var test = require('tape').test;
 
 // As a user, I want to see a zero before I type and every time I press the clear button so that I know calc is on and cache clear
 
 
 test('check clear is zero',function (t) {
+    calculator = new Calculator();
     calculator.clear();
-    var actual = calculator.displayMain;
-    var expected = '0';
-    t.equal(actual, expected);
-    t.end;
-});
-
-
-
-test('add: add two numbers corectly',function (t) {
-    var actual = calculator.calculate(add,'1','2');
-    var expected = '3';
+    var expected = calculator.displayMain;
+    var actual = '0';
     t.equal(actual, expected);
     t.end;
 });
