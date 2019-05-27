4a2e73a4691a476440398d553b30d623824c8883
Jack Bridger <jacksbridger@gmail.com>
2019-05-27 15:56:39 +0100

    setup a test, wrote a failing outline test

--- calculator/new_calc.js
+++ calculator/new_calc.js
@@ -1,5 +1,29 @@
+var test = require('tape').test;
+
+// As a user, I want to see a zero before I type and every time I press the clear button so that I know calc is on and cache clear
 
 
-function add(a, b) {
-    return a +b;
+test('check clear is zero',function (t) {
+    calculator.clear();
+    var actual = calculator.displayMain;
+    var expected = '0';
+    t.equal(actual, expected);
+    t.end;
+});
+
+
+
+test('add: add two numbers corectly',function (t) {
+    var actual = calculator.calculate(add,'1','2');
+    var expected = '3';
+    t.equal(actual, expected);
+    t.end;
+});
+
+var calculator = {
+
+
+    clear() {
+    }
+
 };
--- calculator/new_test.js
+++ calculator/new_test.js
@@ -1,11 +1,23 @@
 var test = require('tape').test;
 
-var add = require('./new_calc.js');
+import {calculator} from 'new_calc.js'
 
-test('add: add two numbers corectly',function (t) {
-    var actual = add(1,2);
-    var expected = 3;
+// As a user, I want to see a zero before I type and every time I press the clear button so that I know calc is on and cache clear
+
+
+test('check clear is zero',function (t) {
+    calculator.clear();
+    var actual = calculator.displayMain;
+    var expected = '0';
     t.equal(actual, expected);
     t.end;
 });
 
+
+
+test('add: add two numbers corectly',function (t) {
+    var actual = calculator.calculate(add,'1','2');
+    var expected = '3';
+    t.equal(actual, expected);
+    t.end;
+});
