e9fe3f6ac16a0cd837184618874b69eb737e6505
Jack Bridger <jacksbridger@gmail.com>
2019-05-29 11:01:54 +0100

    added the operator functions. Now properly displaying only operator after it's clicked. Then only number when operator -> number

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,22 @@
 class Calculator {
     constructor() {
         this.mainDisplay = '0';
+        this.historyDisplay = null;
+        this.firstDigits = null;
+        this.operator = null;
+        this.SecondDigits = null;
     }
 
     update_calc() {
         document.getElementsByClassName('display-main')[0].innerHTML = this.mainDisplay;
+        document.getElementsByClassName('display-history')[0].innerHTML = this.history;
     }
     append_number(buttonPressed) {
         if (this.mainDisplay === '0'){
             this.mainDisplay = buttonPressed;}
+        else if (this.mainDisplay === 'X'|| this.mainDisplay === '-'|| this.mainDisplay === '+'|| this.mainDisplay === '/'){
+                this.mainDisplay = buttonPressed;
+            }
         else if (buttonPressed === '.' && this.mainDisplay.includes('.')) {
             this.mainDisplay = this.mainDisplay;
         }
@@ -22,6 +30,18 @@ class Calculator {
         this.update_calc();
     }
 
+    append_operator(buttonPressed) {
+        if (buttonPressed === '0'||buttonPressed === 'X'|| buttonPressed === '-'|| buttonPressed === '+'|| buttonPressed === '/'){
+            this.mainDisplay = buttonPressed;
+            this.update_calc();
+        }
+        else {
+            this.firstDigits = this.mainDisplay;
+            this.mainDisplay = buttonPressed;
+            this.update_calc();
+        }
+
+    }
 
 
 }
@@ -31,8 +51,6 @@ var calculator = new Calculator();
 window.onload = calculator.update_calc();
 
 const allNumberBtns = document.querySelectorAll('.btn-number');
-
-
 allNumberBtns.forEach(btn =>{
     btn.addEventListener('click', () => { 
         calculator.append_number(btn.innerHTML);
@@ -47,3 +65,10 @@ clearBtn.addEventListener('click',
         calculator.clear();
     }
 );
+
+const allOperatorBtns = document.querySelectorAll('.btn-operator');
+allOperatorBtns.forEach(btn =>{
+    btn.addEventListener('click', () => { 
+        calculator.append_operator(btn.innerHTML);
+    });
+});
