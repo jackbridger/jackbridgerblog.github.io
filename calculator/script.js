1d99783ffcb2ee45a6e0b73e1e8aced0b3129299
Jack Bridger <jacksbridger@gmail.com>
2019-05-29 08:30:06 +0100

    gave up on tests for now. Need to setup browserify or something. Keep getting issues with either using classes in a different file or running node with functions like window.onload

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,16 @@
-window.onload =function() {
-    document.getElementById('hello').innerHTML = 'hello';
-};
+class Calculator {
+    constructor() {
+        this.mainDisplay = '0';
+    }
+
+    update_calc() {
+        document.getElementsByClassName('display-main')[0].innerHTML = this.mainDisplay;
+    }
+
+
+
+}
+
+var calculator = new Calculator();
+
+window.onload = calculator.update_calc();
