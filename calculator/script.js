9faa28b1cbc3586bfeabb5f9c2ff6a2f254f3d09
Jack Bridger <jacksbridger@gmail.com>
2019-05-29 09:45:27 +0100

    number buttons working with longer numbers and adding appropriate decimals

--- calculator/script.js
+++ calculator/script.js
@@ -6,8 +6,16 @@ class Calculator {
     update_calc() {
         document.getElementsByClassName('display-main')[0].innerHTML = this.mainDisplay;
     }
-    append_value(buttonPressed) {
-
+    append_number(buttonPressed) {
+        if (this.mainDisplay === '0'){
+            this.mainDisplay = buttonPressed;}
+        else if (buttonPressed === '.' && this.mainDisplay.includes('.')) {
+            this.mainDisplay = this.mainDisplay;
+        }
+            else {
+            this.mainDisplay += buttonPressed;
+        }
+        this.update_calc();
     }
     clear() {
         this.mainDisplay = '0';
@@ -27,7 +35,7 @@ const allNumberBtns = document.querySelectorAll('.btn-number');
 
 allNumberBtns.forEach(btn =>{
     btn.addEventListener('click', () => { 
-        calculator.mainDisplay = btn.innerHTML;
+        calculator.append_number(btn.innerHTML);
         calculator.update_calc();
     });
 });
