125d92696fdb9fb431ddae817d29bfbd7a3bc9ca
Jack Bridger <jacksbridger@gmail.com>
2019-06-12 11:52:03 +0100

    fixed the error where you couldn't carry one calculation evaluated into the next. Caused by eval() creating a number. Then you couldn't run num.includes(). Converted to string to fix

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,14 @@
 class Calculator {
     // set the initial display values.
     // just returned is if it just performed a calculation (i.e. equals pressed)
-    constructor(/*mainDisplay, historyDisplay, justReturned*/) {
-        this.mainDisplay = /*mainDisplay ||*/ '0';
-        this.historyDisplay = /*historyDisplay ||*/ '';
-        this.justReturned = /*justReturned ||*/false;
+    constructor(mainDisplay, historyDisplay, justReturned) {
+        this.mainDisplay = mainDisplay || '0';
+        this.historyDisplay = historyDisplay || '';
+        this.justReturned = justReturned || false;
     }
     
     // update the view
-    update_view() {
+    update_view(main, history) {
         document.getElementsByClassName('display-main')[0].innerHTML = this.mainDisplay;
         document.getElementsByClassName('display-history')[0].innerHTML = this.historyDisplay;
     }
@@ -37,29 +37,25 @@ class Calculator {
             this.historyDisplay += buttonPressed; 
             this.mainDisplay += buttonPressed;
         }
-        this.update_view();
     }
     // clear the display - reset
     clear() {
         this.historyDisplay = '';
         this.mainDisplay = '0';
-        this.update_view();
     }
     // append operators pressed. Why did I include zero? 
     append_operator(buttonPressed) {
-        if ((buttonPressed === '0'||buttonPressed === '×'|| buttonPressed === '-'|| buttonPressed === '+'|| buttonPressed === '÷') && (this.historyDisplay.includes('+') || this.historyDisplay.includes('-')  || this.historyDisplay.includes('×')  || this.historyDisplay.includes('÷')   )) {
+        if ((buttonPressed === '×'|| buttonPressed === '-'|| buttonPressed === '+'|| buttonPressed === '÷') && (this.historyDisplay.includes('+') || this.historyDisplay.includes('-')  || this.historyDisplay.includes('×')  || this.historyDisplay.includes('÷')   )) {
             this.equals();
             this.mainDisplay = buttonPressed;
-            this.update_view();
         }
-        else if (buttonPressed === '0'||buttonPressed === '×'|| buttonPressed === '-'|| buttonPressed === '+'|| buttonPressed === '÷'){
+        else if (buttonPressed === '×'|| buttonPressed === '-'|| buttonPressed === '+'|| buttonPressed === '÷'){
+            
             this.mainDisplay = buttonPressed;
-            this.update_view();
         }
         else {
             this.firstDigits = this.mainDisplay;
             this.mainDisplay = buttonPressed;
-            this.update_view();
         }
 
     }
@@ -72,11 +68,10 @@ class Calculator {
         toBeCalculated = toBeCalculated.replace('×','*').replace('÷','/');
         var return_val = eval(toBeCalculated);
         return_val = Math.round(return_val * 100000000000)/100000000000;
+        return_val = return_val.toString()
         this.mainDisplay = return_val;
         this.historyDisplay = return_val;
         this.justReturned = true;
-        this.update_view();
-        // calculator = new(this.mainDisplay,this.historyDisplay,true);
     }
 }
 
@@ -93,6 +88,7 @@ const clearBtn = document.querySelector('.btn-ac');
 clearBtn.addEventListener('click', 
     () => {
         calculator.clear();
+        calculator.update_view();
     }
 );
 
@@ -100,6 +96,7 @@ const allOperatorBtns = document.querySelectorAll('.btn-operator');
 allOperatorBtns.forEach(btn =>{
     btn.addEventListener('click', () => { 
         calculator.append_operator(btn.innerHTML);
+        calculator.update_view();
     });
 });
 
@@ -107,6 +104,7 @@ const equalsBtn = document.querySelector('.btn-equals');
 equalsBtn.addEventListener('click', 
     () => {
         calculator.equals()
+        calculator.update_view();
     }
 );
 
